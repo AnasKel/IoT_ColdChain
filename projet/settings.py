e1df301c0a2585f7c0278c69ef576d04e4032d10
AnasKel <anaskelouch2@gmail.com>
2026-01-07 06:14:28 +0100

    Fix email issue

--- projet/settings.py
+++ projet/settings.py
@@ -111,12 +111,12 @@ LOGOUT_REDIRECT_URL = "/login/"
 # EMAIL
 # ======================
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_TIMEOUT = 10
+
 
 # ======================
 # TELEGRAM
