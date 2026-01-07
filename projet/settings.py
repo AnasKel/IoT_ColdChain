333f0a3dfc51ac83d4eb37d6446ec1f2e39a4dd7
AnasKel <anaskelouch2@gmail.com>
2026-01-07 06:00:25 +0100

    Fix email

--- projet/settings.py
+++ projet/settings.py
@@ -111,7 +111,7 @@ LOGOUT_REDIRECT_URL = "/login/"
 # EMAIL
 # ======================
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
